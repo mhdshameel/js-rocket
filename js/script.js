a8877deea507ff4bbf60c9b9ec4a6b9c4d3cc4c6
mhdshameel <mhdshameel@gmail.com>
2018-11-01 21:05:23 +0530

    Added car stuff and js and css

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var sayMyName = function (name) {
+	alert("My name is : " + name);
+}
+
+var car = {
+	color: 'blue',
+	make: 'VW',
+	model: 'Polo',
+	seats: [
+		'seat 1',
+		'seat 2',
+		'seat 3',
+		'seat 4'
+	],
+	isTurnedOn: false,
+	turnOn : function () {
+		this.isTurnedOn = true;
+	},
+	turnOff : function () {
+		this.isTurnedOn = false;
+	},
+	switchCar : function (isOn) {
+		console.log('car is ' + isOn);
+		if (isOn == true)
+			this.isTurnedOn = true;
+		else
+			this.isTurnedOn = false;
+	},
+	fly : function (){
+		alert('flying car');
+	}
+};
+console.log("hello dudes!");
